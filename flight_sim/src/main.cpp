6ba0388dbdc090698cdf2611c0289b8212e170de
sebastianting <143572829+sebastianting@users.noreply.github.com>
2025-09-18 14:43:41 -0400

    add the update loop, updates every 1/60th of second

--- flight_sim/src/main.cpp
+++ flight_sim/src/main.cpp
@@ -4,7 +4,42 @@
  */
 
 #include <stdio.h>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include "physics/rigid_body.hpp"
 
 int main() {
-    printf("Hello\n");
+	RigidBody drone;
+
+	using clock = std::chrono::high_resolution_clock;
+	using duration = std::chrono::duration<double>;
+	
+	const double DELTATIME = 1.0/60.0;	
+	auto previousTime = std::chrono::high_resolution_clock::now(); 
+
+	bool exit = false; 
+
+	std::thread inputThread([&exit](){
+		std:: cin.get();
+		exit = true;
+	});
+
+	//update loop, use enter to exit 
+	while(!exit){
+	
+		auto currentTime = clock::now();
+		duration elapsedTime = currentTime - previousTime;
+		
+		if(elapsedTime.count() >= DELTATIME){
+			//TODO: logic and the actual physics 
+			auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - previousTime);
+			std::cout << "update " << ms.count() << std::endl;
+
+			previousTime = currentTime;
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	}
+
 }
