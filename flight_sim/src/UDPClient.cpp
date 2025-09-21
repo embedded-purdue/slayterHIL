665815c63a8e52e18726a38d472c15bcc5d67131
Benjamin Lobos <blobosle@purdue.edu>
2025-09-21 13:01:01 -0400

    Cleanup

--- flight_sim/src/UDPClient.cpp
+++ flight_sim/src/UDPClient.cpp
@@ -1,47 +1,47 @@
 #include <iostream>
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-  
-#define PORT     8080 
-#define MAXLINE 1024 
-  
-// Driver code 
-int main() { 
-    int sockfd; 
-    char buffer[MAXLINE]; 
-    struct sockaddr_in     servaddr; 
-  
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-  
-    memset(&servaddr, 0, sizeof(servaddr)); 
-      
-    // Filling server information 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(PORT); 
-    servaddr.sin_addr.s_addr = INADDR_ANY; 
-      
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PORT     8080
+#define MAXLINE 1024
+
+// Driver code
+int main() {
+    int sockfd;
+    char buffer[MAXLINE];
+    struct sockaddr_in     servaddr;
+
+    // Creating socket file descriptor
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
     int n;
-    socklen_t len; 
-      
+    socklen_t len;
+
     while (true) {
         std::string line;
         std::cin >> line; // Basic input "scanning"
-        sendto(sockfd, (const char *)line.c_str(), line.size(), 
-        0, (const struct sockaddr *) &servaddr,  
-            sizeof(servaddr)); 
-        std::cout<<"Line sent."<<std::endl; 
+        sendto(sockfd, (const char *)line.c_str(), line.size(),
+        0, (const struct sockaddr *) &servaddr,
+            sizeof(servaddr));
+        std::cout<<"Line sent."<<std::endl;
     }
-    
-  
-    close(sockfd); 
-    return 0; 
+
+
+    close(sockfd);
+    return 0;
 }
