ccde0e3604389a4c86ed91a21eaeefbf88757c21
Craig <ceaglebu@purdue.edu>
2025-09-21 23:47:02 -0400

    Comments in CMakeLists

--- test_node/app/src/main.c
+++ test_node/app/src/main.c
@@ -1,7 +1,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
-#include <threads/example_consumer.h>
-#include <threads/example_producer.h>
+#include "threads/example_consumer.h"
+#include "threads/example_producer.h"
 
 int main(void)
 {
