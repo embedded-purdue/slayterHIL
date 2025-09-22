fbe23f55de2624cda9a6bb23af07b76ae1d3ee36
Craig <ceaglebu@purdue.edu>
2025-09-21 23:39:35 -0400

    Example Producer/Consumer Thread Setup

--- test_node/app/src/main.c
+++ test_node/app/src/main.c
@@ -1,9 +1,14 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+#include <threads/example_consumer.h>
+#include <threads/example_producer.h>
 
 int main(void)
 {
     printk("Hello World! %s\n", CONFIG_BOARD);
+
+    example_consumer_init();
+    example_producer_init();
 
 	return 0;
 }
