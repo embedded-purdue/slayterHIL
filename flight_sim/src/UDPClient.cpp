1b3996ff5f2449c84460b32c08f89b4c66acfa10
frankschlehofer <fschleho@purdue.edu>
2025-09-23 19:25:39 -0400

    Currently can serialize protobuf headers and send them over udp connection

--- flight_sim/src/UDPClient.cpp
+++ flight_sim/src/UDPClient.cpp
@@ -7,13 +7,16 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+#include "../build/sensor_data.pb.h"
+
 #define PORT     8080
 #define MAXLINE 1024
 
 // Driver code
 int main() {
+
+    // Client side setup
     int sockfd;
-    char buffer[MAXLINE];
     struct sockaddr_in     servaddr;
 
     // Creating socket file descriptor
@@ -32,13 +35,85 @@ int main() {
     int n;
     socklen_t len;
 
+
+    float acceleration_x = 0.01f;
+    float acceleration_y = 0.02f;
+    float acceleration_z = -9.81f;
+
+    float angular_velocity_x = 0.03f;
+    float angular_velocity_y = 0.04f;
+    float angular_velocity_z = 0.05f;
+
+    // In Pa
+    float pressure = 123000.11f;
+
+    float altitude = 100.15f;
+
+    // In Celcius
+    float temperature = 22.5f;
+
+    auto packet = std::make_unique<SensorPacket>();
+    
+    // Accessing sub-messages works identically
+    google::protobuf::Timestamp* timestamp = packet->mutable_timestamp();
+    auto now = std::chrono::system_clock::now();
+    auto seconds = std::chrono::time_point_cast<std::chrono::seconds>(now);
+    auto nanoseconds = std::chrono::duration_cast<std::chrono::nanoseconds>(now - seconds);
+    timestamp->set_seconds(seconds.time_since_epoch().count());
+    timestamp->set_nanos(nanoseconds.count());
+    
+    // Populate nested data
+    ImuData* imu = packet->mutable_imu_data();
+    Vector3D* accel = imu->mutable_acceleration();
+    accel->set_x(acceleration_x);
+    accel->set_y(acceleration_y);
+    accel->set_z(acceleration_z);
+
+    Vector3D* gyro = imu->mutable_angular_velocity();
+    gyro->set_x(angular_velocity_x);
+    gyro->set_y(angular_velocity_y);
+    gyro->set_z(angular_velocity_z);
+
+    AltimeterData* altimeter = packet->mutable_altimeter_data();
+    altimeter->set_pressure(pressure);
+    altimeter->set_altitude(altitude);
+    altimeter->set_temperature(temperature);
+
+    printf("Acceleration x,y,z: %f, %f, %f\n", acceleration_x, acceleration_y, acceleration_z);
+    printf("Angular Velocity x,y,z: %f, %f, %f\n", angular_velocity_x, angular_velocity_y, angular_velocity_z);
+    printf("Pressure: %f, Altitude: %f, Temperature: %f\n", pressure, altitude, temperature);
+    
+    // Serialize data into string to send over UDP
+    std::string serialized_data;
+    if (!packet->SerializeToString(&serialized_data)) {
+        std::cerr << "Failed to serialize packet." << std::endl;
+        return -1;
+    }
+
+    printf("Flight sim sending: %s\n", serialized_data.c_str());
+
+    // For testing purposes, try to parse the data
+    SensorPacket parsed_packet;
+    if (!parsed_packet.ParseFromString(serialized_data)) {
+        std::cerr << "Failed to parse packet from string." << std::endl;
+        return -1;
+    }
+
+    float pressure_returned = parsed_packet.altimeter_data().pressure();
+    printf("Pressure deserialized: %f\n", pressure_returned);
+
+
     while (true) {
         std::string line;
-        std::cin >> line; // Basic input "scanning"
-        sendto(sockfd, (const char *)line.c_str(), line.size(),
-        0, (const struct sockaddr *) &servaddr,
-            sizeof(servaddr));
-        std::cout<<"Line sent."<<std::endl;
+        std::cout << "Enter SEND to send packet";
+        std::cin >> line;
+        int bytesSent = sendto(sockfd, (const char *)serialized_data.c_str(), serialized_data.size(),
+                        0, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+        if (bytesSent < 0) {
+            std::cerr << "Failed to send packet." << std::endl;
+            return -1;
+        }
+        printf("Bytes sent: %d\n", bytesSent);
     }
 
 
--- flight_sim/src/UDPServer.cpp
+++ flight_sim/src/UDPServer.cpp
@@ -7,8 +7,10 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+#include "../build/sensor_data.pb.h"
+
 #define PORT (8080)
-#define MAXLINE (1000)
+#define MAXLINE (1024)
 
 int main() {
     int sockfd;
@@ -39,16 +41,25 @@ int main() {
     }
 
     socklen_t len;
-    int n;
 
     len = sizeof(cliaddr);  //len is value/result
 
     while (true) {
-        n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+        
+        int bytesReceived = recvfrom(sockfd, buffer, MAXLINE,
         MSG_WAITALL, ( struct sockaddr *) &cliaddr,
         &len);
-        buffer[n] = '\0';
-        printf("Client sent: %s\n", buffer);
+        printf("Bytes received: %d\n", bytesReceived);
+        //buffer[n] = '\0';
+        printf("Flight sim sent: %s\n", buffer);
+        SensorPacket parsed_packet;
+        // if (!parsed_packet.ParseFromString(buffer)) {
+        //     std::cerr << "Failed to parse packet from string." << std::endl;
+        //     return -1;
+        // }
+        parsed_packet.ParseFromString(buffer);
+        float pressure_returned = parsed_packet.altimeter_data().pressure();
+        printf("Pressure deserialized: %f\n", pressure_returned);
     }
 
     return 0;
