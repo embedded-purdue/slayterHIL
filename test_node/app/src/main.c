ed08e3da9da09c5f33d0252fd55f85e9cb18f712
Aarav Jain <aaravjain2007@gmail.com>
2025-09-28 14:23:07 -0400

    got blinking working

--- test_node/app/src/main.c
+++ test_node/app/src/main.c
@@ -1,9 +1,35 @@
+#include <stdio.h>
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/printk.h>
+
+#define sleep_time  1000
+#define LED0_Node DT_ALIAS(led0)
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_Node, gpios);
 
 int main(void)
 {
-    printk("Hello World! %s\n", CONFIG_BOARD);
+    int ret; 
+    bool led_state = true; 
 
-	return 0;
+    if(!gpio_is_ready_dt(&led)) { 
+        printk("Error: LED device not ready\n");
+        return 0; 
+    }
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if(ret < 0) {
+        printk("Error %d: failed to configure LED pin\n", ret);
+        return 0; 
+    }
+    while(1) {
+         ret = gpio_pin_set_dt(&led, (int)led_state);
+         if(ret < 0) { 
+            printk("Error %d: failed to set LED pin\n", ret);
+            return 0; 
+         }
+         led_state = !led_state; 
+         printk("LED is %s\n", led_state ? "ON" : "OFF");
+         k_msleep(sleep_time);
+    }
+    return 0; 
 }
