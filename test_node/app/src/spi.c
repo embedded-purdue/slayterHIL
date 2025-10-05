8a1c861c9b4fdaadadfd07fc11d4af076623115f
Amber <akkhauv@gmail.com>
2025-10-05 12:24:52 -0400

    initial fail-on-build spi code

---
+++ test_node/app/src/spi.c
@@ -0,0 +1,8 @@
+#include <zephyr/drivers/spi.h>
+
+#define OP (SPI_OP_NODE_MASTER | SPI_TRANSFER_MSB | SPI_WORD_SET(8))
+static const struct spi_dt_spec spi = SPI_DT_SPEC_GET(DT_NODELABEL(spi2), OP, 0);
+
+int main()
+{
+}
