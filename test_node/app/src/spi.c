aae444e6ec90b529fb4fece75fcae004caac03e2
Amber <akkhauv@gmail.com>
2025-10-21 00:19:26 -0400

    added basic spi dt spec get

--- test_node/app/src/spi.c
+++ test_node/app/src/spi.c
@@ -1,9 +1,15 @@
 #include <zephyr/drivers/spi.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/devicetree.h>
 
-#define OP (SPI_OP_NODE_MASTER | SPI_TRANSFER_MSB | SPI_WORD_SET(8))
-static const struct spi_dt_spec spi = SPI_DT_SPEC_GET(DT_NODELABEL(spi2), OP, 0);
+// figure out what flags we want later.
+// we are declaring the esp32 as master right now.
+static const struct spi_dt_spec spi = SPI_DT_SPEC_GET(
+    DT_NODELABEL(esp32),
+    SPI_OP_MODE_MASTER | SPI_WORD_SET(8),
+    0);
 
 int main()
 {
+    printk("%d\n", DT_HAS_ALIAS(myspi));
 }
