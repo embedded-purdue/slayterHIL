3ddb9880490d5ff75d7b2834ffcce7f4f4f7527b
henrychen160905 <henrychen160905@gmail.com>
2025-10-26 14:51:08 -0400

    modified CMakeLists.txt for protobuf dependency and restored main.c

--- flight_controller/app/src/main.c
+++ flight_controller/app/src/main.c
@@ -1,8 +1,8 @@
-#include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
+//#include <zephyr/kernel.h>
+//#include <zephyr/sys/printk.h>
 
 // define thread
-#define STACK_SIZE      2048
+/*#define STACK_SIZE      2048
 #define COMMS_PRIORITY  1
 K_THREAD_STACK_DEFINE(state_machine_stack, STACK_SIZE);
 static struct k_thread state_machine_thread_data;
@@ -11,13 +11,13 @@ static struct k_thread state_machine_thread_data;
 // define message queue
 #define MESSAGES_PER_QUEUE 10
 
-K_MSGQ_DEFINE(event_msgq, sizeof(system_events_t), MESSAGES_PER_QUEUE, alignof(system_events_t));
+K_MSGQ_DEFINE(event_msgq, sizeof(system_events_t), MESSAGES_PER_QUEUE, alignof(system_events_t));*/
 
 int main(void)
 {
 
-    k_thread_create(&state_machine_thread_data, state_machine_stack, STACK_SIZE,
+    /*k_thread_create(&state_machine_thread_data, state_machine_stack, STACK_SIZE,
                     state_machine_thread, NULL, NULL, NULL,
-                    COMMS_PRIORITY, 0, K_NO_WAIT);
+                    COMMS_PRIORITY, 0, K_NO_WAIT);*/
 	return 0;
 }
