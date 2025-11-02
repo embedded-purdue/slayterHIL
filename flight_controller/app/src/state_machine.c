a7b3764d80ef16772490cdab5587c558554f6465
administrator <reachnikhilc@gmail.com>
2025-11-02 15:51:12 -0500

    fixed formatting

--- flight_controller/app/src/state_machine.c
+++ flight_controller/app/src/state_machine.c
@@ -1,31 +1,32 @@
-#include <zephyr/kernel.h>
-#include <stddef.h>
-#include <stdalign.h>
 #include "state_machine.h"
 
-#define MESSAGES_PER_QUEUE 16
-//These are arbitrary sizes for now, may need to be revisited
-#define IMU_CAPACITY       32
-#define LIDAR_CAPACITY     32
-#define COMMAND_CAPACITY   16
+#include <stdalign.h>
+#include <stddef.h>
+#include <zephyr/kernel.h>
 
-#define IMU_SIZE_BYTES     (IMU_CAPACITY * sizeof(imu_state_t))
-#define LIDAR_SIZE_BYTES   (LIDAR_CAPACITY * sizeof(lidar_state_t))
+#define MESSAGES_PER_QUEUE 16
+// These are arbitrary sizes for now, may need to be revisited
+#define IMU_CAPACITY 32
+#define LIDAR_CAPACITY 32
+#define COMMAND_CAPACITY 16
+
+#define IMU_SIZE_BYTES (IMU_CAPACITY * sizeof(imu_state_t))
+#define LIDAR_SIZE_BYTES (LIDAR_CAPACITY * sizeof(lidar_state_t))
 #define COMMAND_SIZE_BYTES (COMMAND_CAPACITY * sizeof(command_t))
 
-//global state transition events
+// global state transition events
 system_events_t event;
 
 // define message queue
 
 K_MSGQ_DEFINE(event_msgq, sizeof(system_events_t), MESSAGES_PER_QUEUE, alignof(system_events_t));
 
-//ring buffer for imu, lidar, and user commands
+// ring buffer for imu, lidar, and user commands
 /*Note: zephyr ring buffers are default "byte mode"
 which means that when you call something like
 ring_buf_put(&imu_buffer, (uint8_t *)&imu_state_t, sizeof(imu_state_t))
 it is copying sizeof(imu_state_t) bytes starting from the second arg mem addr
-and then the same in reverse when you call 
+and then the same in reverse when you call
 ring_buf_get(&imu_buffer, (uint8_t *)&imu_val, sizeof(imu_state_t))
 */
 RING_BUF_DECLARE(imu_buffer, IMU_SIZE_BYTES);
@@ -33,7 +34,7 @@ RING_BUF_DECLARE(lidar_buffer, LIDAR_SIZE_BYTES);
 RING_BUF_DECLARE(command_queue, COMMAND_SIZE_BYTES);
 
 void error(void) {
-    //shutdown motors and abort flight
+    // shutdown motors and abort flight
 }
 
 void init_hw(void) {
@@ -45,7 +46,7 @@ void ascend(void) {
 }
 
 void land(void) {
-    //land and then push a land_done msg
+    // land and then push a land_done msg
 }
 
 void hover(void) {
@@ -53,12 +54,12 @@ void hover(void) {
 }
 
 void controlled_flight(void) {
-    //handles all user commands and then 
+    // handles all user commands and then
 }
 
 // define queue of system events
 void state_machine_handler(system_events_t event) {
-    switch(event) {
+    switch (event) {
         case INIT_HW:
             init_hw();
             break;
@@ -87,8 +88,8 @@ void state_machine_handler(system_events_t event) {
     }
 }
 
-void state_machine_thread(void *p1, void *p2, void *p3) {
-    for(;;) {
+void state_machine_thread(void* p1, void* p2, void* p3) {
+    for (;;) {
         // Thread loop code here
         while (k_msgq_get(&event_msgq, &event, K_FOREVER) == 0) {
             state_machine_handler(event);
