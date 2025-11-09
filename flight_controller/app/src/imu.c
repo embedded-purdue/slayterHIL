94403a4da7ea68da2024ee68be005d7eb6a9ebf6
Aarav Jain <aaravjain2007@gmail.com>
2025-11-09 15:03:04 -0500

    changes to get the message queue working for the imu but it is still in main

--- flight_controller/app/src/imu.c
+++ flight_controller/app/src/imu.c
@@ -1,21 +1,3 @@
-// #include <zephyr/kernel.h>
-// #include <zephyr/sys/printk.h> 
-// #include "state_machine.h"
-
-// // define thread
-// #define STACK_SIZE      2048
-// #define COMMS_PRIORITY  1
-// K_THREAD_STACK_DEFINE(state_machine_stack, STACK_SIZE);
-// static struct k_thread state_machine_thread_data;
-
-// int main(void)
-// {
-//     k_thread_create(&state_machine_thread_data, state_machine_stack, STACK_SIZE,
-//                     state_machine_thread, NULL, NULL, NULL,
-//                     COMMS_PRIORITY, 0, K_NO_WAIT);
-// 	return 0;
-// }
-
 /*
  * Copyright (c) 2019 Nordic Semiconductor ASA
  *
