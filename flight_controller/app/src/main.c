73c54a8f44f7bfa02c61bc84efba5d9c33013fbf
armanislam-07 <armanislamusa@gmail.com>
2026-02-08 11:12:05 -0700

    Initailizing Uart structure

--- flight_controller/app/src/main.c
+++ flight_controller/app/src/main.c
@@ -1,9 +1,10 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h> 
-#include <zephyr/drivers/sensor.h>
 #include "state_machine.h"
 #include "imu.h"
 #include <stdalign.h>
+#include <zephyr/drivers/uart.h>
+#include "uart.h"
 
 // define thread
 #define STACK_SIZE      2048
@@ -15,58 +16,23 @@
 #define IMU_SEM_INIT_COUNT 0 
 #define IMU_SEM_MAX_COUNT 1
 #define IMU_THREAD_PRIORITY 2
+#define UART_CONSUMER_STACK 1024
+#define UART_CONSUMER_PRIORITY 2 
 
-//lidar section
-K_SEM_DEFINE(lidar_sem, 0, 1);
 
-struct lidar_msg {
-    uint16_t distance_mm;
-};
-K_MSGQ_DEFINE(lidar_msgq, sizeof(struct lidar_msg), 8, 4);
-
-/* ======= Timer ISR ======= */
-void lidar_timer_handler(struct k_timer *timer_id)
-{
-    k_sem_give(&lidar_sem);
-}
-K_TIMER_DEFINE(lidar_timer, lidar_timer_handler, NULL);
-
-/* ======= LiDAR Read Thread ======= */
-void lidar_thread(void)
-{
-    const struct device *dev = DEVICE_DT_GET_ONE(grmn_lidar_lite_v4);
-    struct sensor_value val;
-    struct lidar_msg msg;
-
-    if (!device_is_ready(dev)) {
-        printk("LIDAR device not ready!\n");
-        return;
-    }
-
-    while (1) {
-        /* Wait until ISR releases semaphore */
-        k_sem_take(&lidar_sem, K_FOREVER);
-
-        if (sensor_sample_fetch(dev) == 0 &&
-            sensor_channel_get(dev, SENSOR_CHAN_DISTANCE, &val) == 0) {
-            msg.distance_mm = val.val1 * 1000 + val.val2 / 1000;
-            k_msgq_put(&lidar_msgq, &msg, K_NO_WAIT);
-        }
-    }
-}
-K_THREAD_DEFINE(lidar_tid, 2048, lidar_thread, NULL, NULL, NULL,
-                7, 0, 0);
-
-//imu section
 K_THREAD_STACK_DEFINE(state_machine_stack, STACK_SIZE);
+K_THREAD_STACK_DEFINE(uart_consumer_stack, UART_CONSUMER_STACK);
+static struct k_thread uart_consumer_thread_data;
 static struct k_thread state_machine_thread_data;
 
 K_THREAD_STACK_DEFINE(imu_consumer_stack, IMU_STACK_SIZE);
 static struct k_thread imu_consumer_thread_data;
 
 K_MSGQ_DEFINE(imu_msgq, sizeof(struct imu_data), MAX_IMU_MSGQ_LEN, alignof(int));
-K_SEM_DEFINE(imu_sem, IMU_SEM_INIT_COUNT, IMU_SEM_MAX_COUNT)
+K_SEM_DEFINE(imu_sem, IMU_SEM_INIT_COUNT, IMU_SEM_MAX_COUNT);
 K_THREAD_DEFINE(imu_thread, IMU_STACK_SIZE, imu_read_thread, NULL, NULL, NULL, IMU_THREAD_PRIORITY, 0, 0);
+
+K_MSGQ_DEFINE(uart_rx_msgq, sizeof(struct uart_msg), 16, alignof(struct uart_msg));
 
 // Demo app for the IMU Consumer 
 static void imu_consumer(void *arg1, void *arg2, void *arg3) {
@@ -82,37 +48,72 @@ static void imu_consumer(void *arg1, void *arg2, void *arg3) {
         } else {
             printk("Failed to get IMU data: %d\n", rc);
         }
+        
+    }
+}
+//im changing to uart0 to use my computer 
+static void uart_consumer(void *arg1,  void *arg2, void *arg3) {
+    struct uart_msg msg;
+
+    while(1)
+    {
+        if(k_msgq_get(&uart_rx_msgq, &msg, K_FOREVER))
+        {
+            char c = msg.data[0];
+                switch (c) {
+                case 'L':
+                    printk("UART: LEFT\n");
+                    break;
+                case 'R':
+                    printk("UART: RIGHT\n");
+                    break;
+                case 'U':
+                    printk("UART: UP\n");
+                    break;
+                case 'D':
+                    printk("UART: DOWN\n");
+                    break;
+                default:
+                    printk("UART: UNKNOWN '%c'\n", c);
+            }
+        }
     }
 }
 
+
+// int rc1 = k_msgq_put(&uart_rx_msgq, &myMessage, K_NO_WAIT);
+        // if (rc1 == 0)
+        // {
+
+        // }
 int main(void)
 {
-	struct lidar_msg msg;
 
-    printk("Starting LiDAR-Lite V4 test app...\n");
-
-    /* Timer period = 50 ms ⇒ 20 Hz polling rate */
-    k_timer_start(&lidar_timer, K_MSEC(30), K_MSEC(30));
-
+    const struct device *const uart = DEVICE_DT_GET(DT_NODELABEL(uart0));
+    if (!device_is_ready(uart))
+    {
+        printk("uart0 not ready\n");
+        return -1;
+    }
+    uart_irq_callback_user_data_set(uart, uart_callback,&uart_rx_msgq ); //idk what to put for user_data | check if null is okay
+    uart_irq_rx_enable(uart);
+    
     k_thread_create(&state_machine_thread_data, state_machine_stack, STACK_SIZE,
                     state_machine_thread, NULL, NULL, NULL,
                     COMMS_PRIORITY, 0, K_NO_WAIT);
+    k_thread_create(&uart_consumer_thread_data,
+                uart_consumer_stack,
+                UART_CONSUMER_STACK,
+                uart_consumer,
+                NULL, NULL, NULL,
+                UART_CONSUMER_PRIORITY,
+                0,
+                K_NO_WAIT);
 	const struct device *const mpu6050 = DEVICE_DT_GET_ONE(invensense_mpu6050);
     int rc = imu_init(mpu6050);
     if (rc != 0) {
         printk("imu_init failed: %d\n", rc);
     }
     k_thread_create(&imu_consumer_thread_data, imu_consumer_stack, IMU_STACK_SIZE, imu_consumer, NULL, NULL, NULL, IMU_PRIORITY, 0, K_NO_WAIT);
-
-    while (1) {
-        //printk("test to see if we get msg\n");
-        if (k_msgq_get(&lidar_msgq, &msg, K_FOREVER) == 0) {
-            printk("Distance: %u mm\n", msg.distance_mm);
-
-        }
-    }
-    
 	return 0;
-    
-
 }
---
+++ flight_controller/app/src/uart.c
@@ -0,0 +1,74 @@
+  //include your uart.h
+  #include "uart.h"
+  #include <string.h>
+  #include <zephyr/drivers/uart.h>
+  #include <stdalign.h> //try to remove alignof because it is older c 
+
+
+  //intiatie a callback to some state machine 
+
+  /*
+
+  uart_irq_callback_user_data_set ->uart_irq_rx_enable -> make this can define void uart_cb(const struct device *dev, void *user_data) -> uart_irq_update(dev) -> uart_irq_rx_ready(dev) -> uart_irq_rx_ready(dev) - > k_msgq_put(&my_msgq, &msg, K_NO_WAIT)
+
+
+  main():
+    ├─ DEVICE_DT_GET
+    ├─ device_is_ready
+    ├─ uart_irq_callback_user_data_set
+    └─ uart_irq_rx_enable
+
+  UART interrupt occurs
+    └─ uart_cb(dev, user_data)
+        ├─ uart_irq_update
+        ├─ uart_irq_rx_ready
+        ├─ uart_fifo_read (loop)
+        └─ k_msgq_put (K_NO_WAIT)
+
+  worker thread:
+    └─ k_msgq_get
+
+  ISR fires
+  → update irq state
+  → while RX ready:
+        read fifo chunk
+        push chunk to msgq
+
+        what buffer size pass to fifo read ? -> this is how Test_node will interact with us 
+  */
+
+  void uart_callback(const struct device *dev, void *user_data)
+  {
+    uint8_t rx_buf[8]; //this is problem i need a variable 
+    uart_irq_update(dev);
+    if(!uart_irq_rx_ready(dev))
+    {
+      // printk("Uart is not ready\n"); -> find another way to print or call out there is an error 
+      return;
+    } //this is also unneccesaryt 
+    while(uart_irq_rx_ready(dev))
+    {
+      //defensive 
+      //when the fifo_read returns a 0 then we break out of the loo p
+      int bytes_read = uart_fifo_read(dev, rx_buf, sizeof(rx_buf)); //get all the messages in the message buffer 
+      if(bytes_read == 0){
+        break;
+      }
+      
+      struct uart_msg myMessage;
+          
+      myMessage.length = bytes_read;
+
+      memcpy(myMessage.data,rx_buf, bytes_read);
+      
+
+      //is the fifo_read not directly going into my rx_buf ?
+      k_msgq_put(user_data, &myMessage, K_NO_WAIT); //this is not done but i need to define the message quee 
+      /*
+      K_MSGQ_DEFINE(uart_rx_msgq,
+                sizeof(struct uart_msg),
+                QUEUE_LEN,
+                alignof(struct uart_msg)); when i define
+      in ym message queue i use the get funciton which is k_msgq_get with all my data0*/
+    }
+  }
