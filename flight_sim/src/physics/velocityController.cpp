3c7571c7253913b8d7fa71f53eb9322520936c7f
Sebastian Ting <email.sebastiant@gmail.com>
2026-03-01 15:22:21 -0500

    merge

--- flight_sim/src/physics/velocityController.cpp
+++ flight_sim/src/physics/velocityController.cpp
@@ -1,16 +1,6 @@
 #include "velocityController.hpp"
 
 velocityController::velocityController () : 
-<<<<<<< HEAD
-	kp(Eigen::Vector3d::Zero()), ki(Eigen::Vector3d::Zero()), kd(Eigen::Vector3d::Zero()),
-	velocityIntegral(Eigen::Vector3d::Zero()),
-	previousError(Eigen::Vector3d::Zero()),
-  maxForce(0)
-{}
-
-velocityController::velocityController (const Eigen::Vector3d& p, const Eigen::Vector3d& i, const Eigen::Vector3d& d, double maxF) :
-	kp(p), ki(i), kd(d), maxForce(maxF),
-=======
 	kp(0.0), ki(0.0), kd(0.0),
 	velocityIntegral(Eigen::Vector3d::Zero()),
 	previousError(Eigen::Vector3d::Zero())
@@ -18,7 +8,6 @@ velocityController::velocityController (const Eigen::Vector3d& p, const Eigen::V
 
 velocityController::velocityController (double p, double i, double d) :
 	kp(p), ki(i), kd(d),
->>>>>>> a7e1c188b7135259affc3bbdf332b8db098123d3
 	velocityIntegral(Eigen::Vector3d::Zero()),
 	previousError(Eigen::Vector3d::Zero())
 {}
@@ -27,32 +16,10 @@ Eigen::Vector3d velocityController::compute (const Eigen::Vector3d& currentVeloc
 					     const Eigen::Vector3d& targetVelocity, 
 					     double dt) {
 	velocityError = targetVelocity - currentVelocity;
-<<<<<<< HEAD
-
-  Eigen::Vector3d pTerm = kp.cwiseProduct(velocityError);
-  Eigen::Vector3d dTerm = kd.cwiseProduct((velocityError - previousError) / dt);
-  previousError = velocityError;
-
-  Eigen::Vector3d rawOutput = pTerm + ki.cwiseProduct(velocityIntegral) + dTerm;
-
-  if (rawOutput.norm() < maxForce) {
-        velocityIntegral += velocityError * dt;
-  }
-
-  Eigen::Vector3d finalOutput = pTerm + ki.cwiseProduct(velocityIntegral) + dTerm;
-
-  if (finalOutput.norm() > maxForce) {
-        finalOutput = finalOutput.normalized() * maxForce;
-  }
-
-  return finalOutput;
-=======
 	velocityIntegral += velocityError * dt;
 	Eigen::Vector3d velocityDerivative = (velocityError - previousError) /dt;
 	previousError = velocityError;
 
 	return kp * velocityError + ki * velocityIntegral + kd * velocityDerivative;
 
->>>>>>> a7e1c188b7135259affc3bbdf332b8db098123d3
 }
-
