2210843cba1b325d2f2c04700e671f2c36348b3e
Sebastian Ting <email.sebastiant@gmail.com>
2026-03-01 16:47:26 -0500

    imu faking

--- flight_sim/src/data_faking/imu_generation.hpp
+++ flight_sim/src/data_faking/imu_generation.hpp
@@ -1,1 +1,48 @@
-#
+#// imu_sim.h
+
+#ifndef IMU_SIM_H
+#define IMU_SIM_H
+
+#define EULER_SCALE  16.0f
+#define ACCEL_SCALE  100.0f
+#define GYRO_SCALE   16.0f
+
+#include <stdint.h>
+
+// --- IMU hardware struct definitions ---
+typedef struct {
+    int8_t lsb;
+    int8_t msb;
+} i2c_imu_data_16_t;
+
+typedef struct {
+    i2c_imu_data_16_t x;
+    i2c_imu_data_16_t y;
+    i2c_imu_data_16_t z;
+} i2c_imu_triplet_t;
+
+typedef struct {
+    i2c_imu_triplet_t euler_angles;
+    i2c_imu_triplet_t linear_acceleration;
+    i2c_imu_triplet_t gyro;
+} imu_data_t;
+
+class ImuSimulator {
+public:
+    ImuSimulator(float mass, float moment_of_inertia);
+    imu_data_t update(float fx, float fy, float fz,
+                      float tx, float ty, float tz,
+                      float dt);
+
+private:
+    float m_;
+    float I_;
+
+    float wx_, wy_, wz_;
+    float roll_, pitch_, yaw_;
+
+    i2c_imu_data_16_t pack_16(int16_t value);
+};
+
+
+#endif
