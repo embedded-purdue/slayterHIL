e4345741c25ba5f32ab92f05e7d3048e70f19ec9
Aarav Jain <aaravjain2007@gmail.com>
2026-03-07 03:25:39 -0500

    all of the thread and stack definitions

--- flight_controller/app/src/main.c
+++ flight_controller/app/src/main.c
@@ -4,93 +4,11 @@
 #include "state_machine.h"
 #include "imu.h"
 #include "lidar.h"
+#include "bno.h"
 #include "uart.h"
 #include <stdio.h> 
 #include <stdalign.h>
 #include <bno055.h>
-// // define thread
-// #define STACK_SIZE      2048
-// #define COMMS_PRIORITY  1
-// #define IMU_STACK_SIZE 1024
-// #define IMU_PRIORITY   3
-// #define MAX_IMU_MSGQ_LEN 16
-// #define IMU_STACK_SIZE 1024
-// #define IMU_SEM_INIT_COUNT 0 
-// #define IMU_SEM_MAX_COUNT 1
-// #define IMU_THREAD_PRIORITY 2
-
-// //lidar section
-// K_SEM_DEFINE(lidar_sem, 0, 1);
-
-// struct lidar_msg {
-//     uint16_t distance_mm;
-// };
-// K_MSGQ_DEFINE(lidar_msgq, sizeof(struct lidar_msg), 8, 4);
-
-// /* ======= Timer ISR ======= */
-// void lidar_timer_handler(struct k_timer *timer_id)
-// {
-//     k_sem_give(&lidar_sem);
-// }
-// K_TIMER_DEFINE(lidar_timer, lidar_timer_handler, NULL);
-
-// /* ======= LiDAR Read Thread ======= */
-// void lidar_thread(void)
-// {
-//     const struct device *dev = DEVICE_DT_GET_ONE(grmn_lidar_lite_v4);
-//     struct sensor_value val;
-//     struct lidar_msg msg;
-
-//     if (!device_is_ready(dev)) {
-//         printk("LIDAR device not ready!\n");
-//         return;
-//     }
-
-//     while (1) {
-//         /* Wait until ISR releases semaphore */
-//         k_sem_take(&lidar_sem, K_FOREVER);
-
-//         if (sensor_sample_fetch(dev) == 0 &&
-//             sensor_channel_get(dev, SENSOR_CHAN_DISTANCE, &val) == 0) {
-//             msg.distance_mm = val.val1 * 1000 + val.val2 / 1000;
-//             k_msgq_put(&lidar_msgq, &msg, K_NO_WAIT);
-//         }
-//     }
-// }
-// K_THREAD_DEFINE(lidar_tid, 2048, lidar_thread, NULL, NULL, NULL,
-//                 7, 0, 0);
-
-// //imu section
-// K_THREAD_STACK_DEFINE(state_machine_stack, STACK_SIZE);
-// static struct k_thread state_machine_thread_data;
-
-// K_THREAD_STACK_DEFINE(imu_consumer_stack, IMU_STACK_SIZE);
-// static struct k_thread imu_consumer_thread_data;
-
-// K_MSGQ_DEFINE(imu_msgq, sizeof(struct imu_data), MAX_IMU_MSGQ_LEN, alignof(int));
-// K_SEM_DEFINE(imu_sem, IMU_SEM_INIT_COUNT, IMU_SEM_MAX_COUNT)
-// K_THREAD_DEFINE(imu_thread, IMU_STACK_SIZE, imu_read_thread, NULL, NULL, NULL, IMU_THREAD_PRIORITY, 0, 0);
-
-// // Demo app for the IMU Consumer 
-// static void imu_consumer(void *arg1, void *arg2, void *arg3) {
-//     struct imu_data out; 
-//     while (1) {
-//         // struct imu_data out;
-//         int rc = imu_get(&out, K_FOREVER);
-//         if (rc == 0) {
-//             printk("[%s] Temp: %.2f C, Accel: [%.2f, %.2f, %.2f] m/s², Gyro: [%.2f, %.2f, %.2f] dps\n",
-//                    now_str(), out.temp,
-//                    out.accel[0], out.accel[1], out.accel[2],
-//                    out.gyro[0], out.gyro[1], out.gyro[2]);
-//         } else {
-//             printk("Failed to get IMU data: %d\n", rc);
-//         }
-//     }
-// }
-
-
-
-
 
 
 static const struct device *bno055_dev = DEVICE_DT_GET(DT_NODELABEL(bno)); 
@@ -102,11 +20,11 @@ static const struct device *bno055_dev = DEVICE_DT_GET(DT_NODELABEL(bno));
 #define IMU_PRIORITY   3
 #define LIDAR_STACK_SIZE 1024
 #define LIDAR_PRIORITY 4
-#define MAX_IMU_MSGQ_LEN 16
+#define MAX_BNO_MSGQ_LEN 16
 #define MAX_LIDAR_MSGQ_LEN 8
-#define IMU_SEM_INIT_COUNT 0 
-#define IMU_SEM_MAX_COUNT 1
-#define IMU_THREAD_PRIORITY 2
+#define BNO_SEM_INIT_COUNT 0 
+#define BNO_SEM_MAX_COUNT 1
+#define BNO_THREAD_PRIORITY 2
 #define UART_CONSUMER_STACK 1024
 #define UART_CONSUMER_PRIORITY 5
 
@@ -123,15 +41,15 @@ static struct k_thread lidar_consumer_thread_data;
 K_THREAD_STACK_DEFINE(state_machine_stack, STACK_SIZE);
 static struct k_thread state_machine_thread_data;
 
-K_THREAD_STACK_DEFINE(imu_consumer_stack, IMU_STACK_SIZE);
-static struct k_thread imu_consumer_thread_data;
+K_THREAD_STACK_DEFINE(BNO_consumer_stack, IMU_STACK_SIZE);
+static struct k_thread BNO_consumer_thread_data;
 
 
-K_MSGQ_DEFINE(imu_msgq, sizeof(struct imu_data), MAX_IMU_MSGQ_LEN, alignof(int));
-K_SEM_DEFINE(imu_sem, IMU_SEM_INIT_COUNT, IMU_SEM_MAX_COUNT);
+K_MSGQ_DEFINE(BNO_msgq, sizeof(struct BNO_data), MAX_BNO_MSGQ_LEN, alignof(int));
+K_SEM_DEFINE(BNO_sem, BNO_SEM_INIT_COUNT, BNO_SEM_MAX_COUNT);
 
 
-K_THREAD_DEFINE(imu_thread, IMU_STACK_SIZE, imu_read_thread, NULL, NULL, NULL, IMU_THREAD_PRIORITY, 0, 0);
+K_THREAD_DEFINE(BNO_thread, IMU_STACK_SIZE, BNO_read_thread, NULL, NULL, NULL, BNO_THREAD_PRIORITY, 0, 0);
 K_THREAD_DEFINE(lidar_thread, LIDAR_STACK_SIZE, lidar_read_thread, NULL, NULL, NULL, LIDAR_PRIORITY, 0, 0);
 
 //uart section
@@ -141,19 +59,19 @@ K_MSGQ_DEFINE(uart_rx_msgq, sizeof(struct uart_msg), 16, alignof(struct uart_msg
 
 
 // Demo app for the IMU Consumer 
-static void imu_consumer(void *arg1, void *arg2, void *arg3) {
-    struct imu_data out; 
-    printk("IMU consumer thread started\n");
+static void BNO_consumer(void *arg1, void *arg2, void *arg3) {
+    struct BNO_data out; 
+    printk("BNO consumer thread started\n");
     
     while (1) {
-        int rc = imu_get(&out, K_FOREVER);
+        int rc = BNO_get(&out, K_FOREVER);
         if (rc == 0) {
             printk("[%s] Temp: %.2f C, Accel: [%.2f, %.2f, %.2f] m/s², Gyro: [%.2f, %.2f, %.2f] dps\n",
                    now_str(), out.temp,
                    out.accel[0], out.accel[1], out.accel[2],
                    out.gyro[0], out.gyro[1], out.gyro[2]);
         } else {
-            printk("Failed to get IMU data: %d\n", rc);
+            printk("Failed to get BNO data: %d\n", rc);
         }
         k_msleep(100); // Add small delay to prevent spam
     }
