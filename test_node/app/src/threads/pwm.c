ac1e08b3b9fae5bf5a6eb67a0bac8a4caf83c16a
kerryzhang50 <zkerry50@gmail.com>
2026-04-12 14:23:15 -0400

    added i2c4 in overlay, added extra timers for motors, added 3 extra pwm variables for each motor, removed mutex stuff from imu_emulator

--- test_node/app/src/threads/pwm.c
+++ test_node/app/src/threads/pwm.c
@@ -5,22 +5,41 @@
 
 LOG_MODULE_REGISTER(pwm_thread, LOG_LEVEL_INF);
 
-const struct device *pwm_input_dev = DEVICE_DT_GET(DT_ALIAS(pwm_input));
+const struct device *pwm_input_dev1 = DEVICE_DT_GET(DT_ALIAS(pwm_input1));
+const struct device *pwm_input_dev2 = DEVICE_DT_GET(DT_ALIAS(pwm_input2));
+const struct device *pwm_input_dev3 = DEVICE_DT_GET(DT_ALIAS(pwm_input3));
+const struct device *pwm_input_dev4 = DEVICE_DT_GET(DT_ALIAS(pwm_input4));
 const struct device *pwm_output_dev = NULL; // TODO: assign correct device
 
 K_THREAD_STACK_DEFINE(pwm_stack, PWM_STACK_SIZE);
 struct k_thread pwm_thread_data;
 
 void pwm_thread(void*, void*, void*) {
-    uint64_t pwm_period;
-    uint64_t pwm_pulse;
+    uint64_t pwm_period1;
+    uint64_t pwm_pulse1;
+    uint64_t pwm_period2;
+    uint64_t pwm_pulse2;
+    uint64_t pwm_period3;
+    uint64_t pwm_pulse3;
+    uint64_t pwm_period4;
+    uint64_t pwm_pulse4;
+
     while(1) {
-        if(pwm_capture_usec(pwm_input_dev, 0, &pwm_period, &pwm_pulse) == 0) {
+        if(pwm_capture_usec(pwm_input_dev1, 0, &pwm_period1, &pwm_pulse1) == 0 &&
+           pwm_capture_usec(pwm_input_dev2, 0, &pwm_period2, &pwm_pulse2) == 0 &&
+           pwm_capture_usec(pwm_input_dev3, 0, &pwm_period3, &pwm_pulse3) == 0 &&
+           pwm_capture_usec(pwm_input_dev4, 0, &pwm_period4, &pwm_pulse4) == 0) {
             // dummy printing for sanity
             LOG_INF("PWM captured: period = %llu us, pulse = %llu us", pwm_period, pwm_pulse);
 
-            double duty_cycle = (double)pwm_pulse / (double)pwm_period;
-            double avg_voltage = duty_cycle * VOLTAGE_HIGH;
+            double duty_cycle1 = (double)pwm_pulse1 / (double)pwm_period1;
+            double avg_voltage1 = duty_cycle1 * VOLTAGE_HIGH;
+            double duty_cycle2 = (double)pwm_pulse2 / (double)pwm_period2;
+            double avg_voltage2 = duty_cycle2 * VOLTAGE_HIGH;
+            double duty_cycle3 = (double)pwm_pulse3 / (double)pwm_period3;
+            double avg_voltage3 = duty_cycle3 * VOLTAGE_HIGH;
+            double duty_cycle4 = (double)pwm_pulse4 / (double)pwm_period4;
+            double avg_voltage4 = duty_cycle4 * VOLTAGE_HIGH;
             
             // eventually, we will send the output on the output device, but we don't
             // have the output device set up yet
@@ -31,7 +50,10 @@ void pwm_thread(void*, void*, void*) {
 }
 
 void pwm_init() {
-    if (!device_is_ready(pwm_input_dev)) {
+    if (!device_is_ready(pwm_input_dev1) ||
+        !device_is_ready(pwm_input_dev2) ||
+        !device_is_ready(pwm_input_dev3) ||
+        !device_is_ready(pwm_input_dev4)) {
         LOG_ERR("PWM input device is not ready");
         return;
     }
