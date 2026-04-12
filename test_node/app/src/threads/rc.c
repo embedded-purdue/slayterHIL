4dc2164b55980cdc933e870500df2bba955ba6de
Evin Lodder <evinlodder@gmail.com>
2026-04-12 13:56:41 -0400

    changed order per aarav, also added initial up command

--- test_node/app/src/threads/rc.c
+++ test_node/app/src/threads/rc.c
@@ -18,16 +18,17 @@ bool rc_command_received(rc_data_t command) {
     // atomic_set(&latest_rc_command, (atomic_t));
     // send the command over the UART interface
     // 'I' means idle
-    // order should (i think) be 1. [L/R] 2. [F/B] 3. [U/D]
-    if(command.rc_horiz == 'F' || command.rc_horiz == 'B') {
+    // order should (i think) be 1. [U/D] 2. [L/R] 3. [F/B]
+
+    uart_poll_out(uart_dev, command.rc_vert);
+
+    if(command.rc_horiz == 'L' || command.rc_horiz == 'R') {
         uart_poll_out(uart_dev, 'I');
         uart_poll_out(uart_dev, command.rc_horiz);
     } else {
         uart_poll_out(uart_dev, command.rc_horiz);
         uart_poll_out(uart_dev, 'I');
     }
-
-    uart_poll_out(uart_dev, command.rc_vert);
 
     return true;
 }
@@ -38,6 +39,9 @@ int rc_init(const struct device* dev) {
         LOG_ERR("UART device for RC is not ready");
         return -1;
     }
+
+    // send an initial UP command to DUT to start in hover state
+    uart_poll_out(dev, 'U');
 
     uart_dev = dev;
     return 0;
